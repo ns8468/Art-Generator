22f11d83712609ecd4ceac24850ab8ff42d1ef38
Nihal Shah Parekkattil <82871834+ns8468@users.noreply.github.com>
2021-09-15 16:52:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+/*
+loaded these files from html
+
+https://assets.codepen.io/3919397/dat.gui.min.js
+https://assets.codepen.io/3919397/simplex-noise.js
+https://assets.codepen.io/3919397/utils.js
+https://assets.codepen.io/3919397/datInstance.js
+https://assets.codepen.io/3919397/effect.js
+https://assets.codepen.io/3919397/shapes.js
+https://assets.codepen.io/3919397/main.js
+*/
